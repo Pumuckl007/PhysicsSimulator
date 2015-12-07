bec73a571944002cddf20ad543e8cccaa2944360
Pumuckl007 <example@example.com>
2015-12-06 20:24:05 -0800

    got pendulums to generate

--- js/World.js
+++ js/World.js
@@ -20,7 +20,7 @@ class World {
   }
 
   addGrid(){
-    let size = 40, step = 1;
+    let size = 80, step = 1;
 
     let geometry = new THREE.Geometry();
     let material = new THREE.LineBasicMaterial( { color: 0xcccccc, opacity: 0.2 } );
@@ -40,7 +40,7 @@ class World {
   }
 
   static G (){
-    return 9.81;
+    return -9.81;
   }
 
   render (){
@@ -73,7 +73,7 @@ class PhysicsObject {
   }
 
   getMesh (){
-    let gemometry = new THREE.SphereGeometry( this.mass/5, 32, 32 );
+    let gemometry = new THREE.SphereGeometry( Math.pow(this.mass, 1/3)/5, 32, 32 );
     let material = new THREE.MeshPhongMaterial( { color:0xaaaaaa, specular: 0x555555, shininess: 30});
     let mesh = new THREE.Mesh(gemometry, material);
     mesh.position.x = this.x;
@@ -95,7 +95,7 @@ class Pendulum extends PhysicsObject{
     super(x,y,z);
     this.length = length;
     this.bob = bob;
-    this.forceRenderer = new ForceRenderer([]);
+    this.forceRenderer = new ForceRenderer([], 0.1*this.bob.mass, 0.03);
   }
 
   update (){
@@ -103,7 +103,7 @@ class Pendulum extends PhysicsObject{
   }
 
   getMesh (){
-    let gemometry = new THREE.CylinderGeometry( 0.03, 0.03, this.length, 4 );
+    let gemometry = new THREE.CylinderGeometry( 0.03, 0.03, this.length, 12 );
     gemometry.translate(0, -this.length/2, 0);
     let material = new THREE.MeshPhongMaterial( { color:0xcf9458, specular: 0x555555, shininess: 30});
     let mesh = new THREE.Mesh(gemometry, material);
@@ -123,7 +123,11 @@ class Pendulum extends PhysicsObject{
 class SimplePendulum extends Pendulum{
     constructor(length, x, y, z, bob, initialTheta){
       super(length, x, y, z, bob);
-      this.initialTheta = initialTheta;
+      this.initialTheta = Math.PI/180*initialTheta;
+      // this.forceRenderer.updateForce(0, new THREE.Vector3(1, 1, 0), 0);
+      this.forceRenderer.updateForce(1, new THREE.Vector3(0, World.G(), 0), 0x0000FF);
+      // this.forceRenderer.updateForce(2, new THREE.Vector3(-1, 1, 0), 0);
+      this.forceRenderer.buildMesh();
     }
 
     update(){
@@ -132,6 +136,10 @@ class SimplePendulum extends Pendulum{
       this.stringMesh.position.set(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z);
       this.bobMesh.position.set(this.posX(), this.posY() , this.posZ());
       super.update();
+      this.forceRenderer.updateForce(0, new THREE.Vector3(0, this.yAcc()-World.G(), 0), 0x00FF00);
+      this.forceRenderer.updateForce(2, new THREE.Vector3(this.xAcc(), 0, 0), 0xFF0000);
+      this.forceRenderer.buildMesh();
+      this.forceRenderer.setPosition(this.bobMesh.position.x, this.bobMesh.position.y, this.bobMesh.position.z);
     }
 
     angle(){
@@ -139,7 +147,7 @@ class SimplePendulum extends Pendulum{
     }
 
     periodTime(){
-      return 2*Math.PI*Math.sqrt(this.length / World.G());
+      return 2*Math.PI*Math.sqrt(this.length / Math.abs(World.G()));
     }
 
     xVel(){
@@ -156,6 +164,26 @@ class SimplePendulum extends Pendulum{
       let second = -this.initialTheta*Math.sin(inside);
       let thired = 2*Math.PI/this.periodTime();
       return first * second * thired;
+    }
+
+    xAcc(){
+      let period = this.periodTime();
+      let pitheta = -4*Math.PI*Math.PI*this.initialTheta/period/period;
+      let twoPiTPeriod = 2*Math.PI*world.time/period;
+      let cos = Math.cos(twoPiTPeriod);
+      let thetaCos = this.initialTheta*cos;
+      let sin = Math.sin(twoPiTPeriod);
+      return pitheta*(this.initialTheta*sin*sin*Math.sin(thetaCos)+ cos*Math.cos(thetaCos));
+    }
+
+    yAcc(){
+      let period = this.periodTime();
+      let pitheta = 4*Math.PI*Math.PI*this.initialTheta/period/period;
+      let twoPiTPeriod = 2*Math.PI*world.time/period;
+      let cos = Math.cos(twoPiTPeriod);
+      let thetaCos = this.initialTheta*cos;
+      let sin = Math.sin(twoPiTPeriod);
+      return -pitheta*(this.initialTheta*cos*Math.sin(thetaCos)- sin*sin*Math.cos(thetaCos));
     }
 
     posX(){
@@ -175,13 +203,15 @@ class ForceRenderer{
   constructor(forces, scale, width){
     this.forces = forces;
     this.mesh = new THREE.Object3D();
-    this.foward = new Three.Vector3(0,1,0);
+    this.foward = new THREE.Vector3(0,1,0);
     this.scale = scale;
     this.width = width;
+    this.meshMap = [];
   }
 
-  updateForce(name, vector){
-    this.forces[name] = vector;
+  updateForce(index, vector, color){
+    this.forces[index] = [vector, color];
+    vector.indexHelper = index;
   }
 
   setRotation(x, y, z){
@@ -191,17 +221,34 @@ class ForceRenderer{
     }
   }
 
+  setPosition(x, y, z){
+    this.mesh.position.set(x,y,z);
+    for(let mesh of this.mesh.children){
+      mesh.position.set(x,y,z);
+    }
+  }
+
   buildMesh(){
-    this.mesh.children = [];
-    for(let force of this.forces){
-        let geomBody = new THREE.CylinderGeometry(this.width, this.width, force.length()*this.scale, 6, 1, true);
-        geomBody.translate(0, force.length/2*this.scale, 0);
-        let geomHead = new THREE.CylinderGeometry(this.width*2, 0, force.length/6*this.scale, 6);
-        geomHead.translate(0, 11*force.length/12*this.scale, 0);
-        let geom = GeometryUtils.merge(geomBody, geomHead);
-        let norm = force.clone.normalize();
-        let mat = new THREE.MeshBasicMaterial(Math.floor(0xFF*(norm.x+1)/2)*0x10000 + Math.floor(0xFF*(norm.y+1)/2)*0x100 + Math.floor(0xFF*(norm.x+1)/2));
-        this.mesh.children.push(new THREE.Mesh(geom, mat));
+    for(let comb of this.forces){
+      if(comb){
+        let force = comb[0];
+        if(this.meshMap[force.indexHelper]){
+          let mesh = this.meshMap[force.indexHelper];
+          mesh.rotation.z = ((force.y === 0) ? ((force.x > 0) ? 0 : Math.PI) : ((force.x < 0) ? Math.PI : 0) + Math.atan(force.y / force.x)) - Math.PI/2;
+          mesh.scale.set(1, force.length()*this.scale, 1);
+          mesh.material.color.setHex(comb[1]);
+        } else {
+          let norm = force.clone().normalize();
+          let mat = new THREE.MeshBasicMaterial({color:comb[1]});
+          let geom = new THREE.CylinderGeometry(this.width, this.width, 1, 6);
+          geom.translate(0, 0.5, 0);
+          let mesh = new THREE.Mesh(geom, mat);
+          mesh.rotation.z = ((force.y === 0) ? ((force.x > 0) ? 0 : Math.PI) : ((force.x < 0) ? Math.PI : 0) + Math.atan(force.y / force.x)) - Math.PI/2;
+          mesh.scale.set(1, force.length()*this.scale, 1);
+          this.mesh.children.push(mesh);
+          this.meshMap[force.indexHelper] = mesh;
+        }
+      }
     }
   }
 
@@ -229,9 +276,14 @@ class ForceRenderer{
 
 var world = new World();
 world.addGrid();
-var bob = new Bob(1);
-var pendulum = new SimplePendulum(1, 0, 1, 0, bob, 1);
-world.addObject(pendulum);
+for(let i = 0; i < 25; i++){
+  for(let k = 0; k < 25; k++){
+    let bob = new Bob(5);
+    let x = Math.random()*6.5+0.5;
+    let pendulum = new SimplePendulum(x, (k-10)*5, x, (i-10)*5, bob, Math.random()*85);
+    world.addObject(pendulum);
+  }
+}
 var step = function(){
   requestAnimationFrame(step);
   world.update();
